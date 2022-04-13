2872993a56917e1af4581cacaa8d71c3b30be672
David seco <deivid.develop@gmail.com>
2022-04-13 09:54:35 +0200

    Add react-router-dom V6

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,63 @@
 import React  from 'react';
 import { useState, useEffect } from 'react'
-import { BrowserRouter, Route, Link, Navigate, Routes } from 'react-router-dom';
 
 
 import logo from './logo.svg'
 import './App.css'
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
+
 import LoginComponent from './components/login/LoginComponent';
+import RegisterComponent from './components/register/RegisterComponent';
 
 function App() {
   const [isLogged, setLogged] = useState(false);
 
   useEffect(() => {
-    setLogged(false);
+    setLogged(true);
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <h1 className="text-3xl font-bold text-blue-600">Paco</h1>
-        <p className="foo-text">
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-      <main>
-      
+    <Router>
       <Routes>
-          <Route path="login" element={<LoginComponent />} />
-          {/* {isLogged ? <Navigate to="/"/>  : <Navigate to="/login" />} */}
-          {/* <Route path="/login" element={<LoginComponent />}/> */}
-
-          {/* { isLogged
-          ?  <Navigate replace to="/" />
-          :  <Navigate replace to="login" />
-        } */}
+        {!isLogged && <Route path="/" element={<Navigate to="/login"/>}/> } 
+        <Route path="/" element={  <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hello Vite + React!</p>
+          <h1 className="text-3xl font-bold text-blue-600">Paco</h1>
+          <p className="foo-text">
+            Edit <code>App.tsx</code> and save to test HMR updates.
+          </p>
+          <p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            {' | '}
+            <a
+              className="App-link"
+              href="https://vitejs.dev/guide/features.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Vite Docs
+            </a>
+          </p>
+        </header>
+        </div>} 
+        />
+        <Route path="login" element={<LoginComponent />} />
       </Routes>
-
-      </main>
-    </div>
+    </Router>
   )
 }
 
---
+++ src/components/register/RegisterComponent.tsx
@@ -0,0 +1,8 @@
+const RegisterComponent = () => {
+    return (
+        <form>
+            <h1 className="text-2xl font bold text-blue-500">Register</h1>
+        </form>
+    )
+}
+export default RegisterComponent;
